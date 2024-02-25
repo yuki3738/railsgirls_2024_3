239f3b40af839a18d3685a9fe2415f442e194e7f
yuki3738 <y.minamiya@gmail.com>
2024-02-26 01:27:15 +0900

    Change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: redirect("/ideas")
+
   resources :ideas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
